d1bda4fc4fb9cbe4d2a7caae8a15b0319f8bf930
Bane.Shi <fengxiaotx@163.com>
2016-05-02 20:35:20 +0800

    [Bane.Shi]

---
+++ src/MoenSun/Ueditor/MSUeditorServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by Bane.Shi.
+ * Copyright MoenSun
+ * User: Bane.Shi
+ * Date: 16/5/2
+ * Time: 20:27
+ */
+
+namespace MoenSun\Ueditor;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class MSUeditorServiceProvider extends ServiceProvider
+{
+    protected $defer = false;
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+
+    public function boot(){
+
+    }
+}
